f9286627e46e8f449f661bfaae382d54981f3b04
Azahast <weyderfs@gmail.com>
2020-07-18 19:33:50 -0300

    feature/develop - Continuing developing DocumentDB Module

--- documentdb/cluster_instance/main.tf
+++ documentdb/cluster_instance/main.tf
@@ -1,13 +1,36 @@
 resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = var.cluster_instances_count
-  identifier         = var.cluster_instances_identifier
-  cluster_identifier = var.cluster_instances_cluster_identifier
-  instance_class     = var.cluster_instances_instance_class
+
+  count                         = var.cluster_instances_count
+  identifier                    = var.cluster_instances_identifier
+  cluster_identifier            = var.cluster_instances_cluster_identifier
+  instance_class                = var.cluster_instances_instance_class
+  auto_minor_version_upgrade    = var.cluster_instance_auto_minor_version_upgrade
+  engine                        = var.cluster_instance_engine
+  identifier_prefix             = var.cluster_instances_identifier_prefix
+  instance_class                = var.cluster_instances_instance_class
+  preferred_maintenance_window  = var.cluster_instances_preferred_maintenance_window
+  promotion_tier                = var.cluster_instances_promotion_tier
+  arn                           = var.cluster_instances_arn
+  db_subnet_group_name          = var.cluster_instances_db_subnet_group_name
+  dbi_resource_id               = var.cluster_instances_dbi_resource_id
+  endpoint                      = var.cluster_instances_endpoint
+  engine_version                = var.cluster_instances_engine_version
+  kms_key_id                    = var.cluster_identifier_kms_key_id
+  port                          = var.cluster_identifier_port
+  preferred_backup_window       = var.cluster_identifier_preferred_backup_window
+  storage_encrypted             = var.cluster_identifier_storage_encrypted
+  writer                        = var.cluster_identifier_writer
+  ca_cert_identifier            = var.cluster_identifier_ca_cert_identifier
+  
+
 }
 
 resource "aws_docdb_cluster" "cluster" {
-  cluster_identifier = var.cluster_identifier
-  availability_zones = var.cluster_availability_zones
-  master_username    = var.cluster_master_username
-  master_password    = var.cluster_master_password
+
+  cluster_identifier          = var.cluster_identifier
+  availability_zones          = var.cluster_availability_zones
+  master_username             = var.cluster_master_username
+  master_password             = var.cluster_master_password
+
+
 }
---
+++ documentdb/cluster_instance/variables.tf
@@ -0,0 +1,125 @@
+variable "cluster_instances_count" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_instances_identifier" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_instances_cluster_identifier" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_instances_instance_class" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_instance_auto_minor_version_upgrade" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_instance_engine" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_instances_identifier_prefix" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_instances_instance_class" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_instances_preferred_maintenance_window" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_instances_promotion_tier" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_instances_arn" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_instances_db_subnet_group_name" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_instances_dbi_resource_id" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_instances_endpoint" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_instances_engine_version" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_identifier_kms_key_id" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_identifier_port" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_identifier_preferred_backup_window" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_identifier_storage_encrypted" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_identifier_writer" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_identifier_ca_cert_identifier" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_identifier" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_availability_zones" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_master_username" {
+    type        = 
+    default     =
+    description = 
+}
+variable "cluster_master_password" {
+    type        = 
+    default     =
+    description = 
+}
