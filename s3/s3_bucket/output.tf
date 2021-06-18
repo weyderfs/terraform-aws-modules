f62541bb8d0f0fe450266983e800224443403711
Weyder Santos <weyder.santos@guiabolso.com.br>
2021-06-18 15:21:40 -0300

    fix path

--- s3/s3_bucket/output.tf
+++ s3/s3_bucket/output.tf
@@ -2,8 +2,3 @@ output "s3_arn" {
   value       = aws_s3_bucket.s3.arn
   description = "s3_arn"
 }
-
-output "bucket" {
-  value       = aws_s3_bucket.s3.bucket
-  description = "Name of Bucket"
-}
