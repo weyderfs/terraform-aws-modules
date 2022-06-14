28eb785fc24fbbe9e22f93e6f79a15bfb8680ca2
Weyder Ferreira <weyder.santos@picpay.com>
2022-06-13 23:10:19 -0300

    removendo itens duplicados

--- secrets-manager/secrets/main.tf
+++
@@ -1,7 +0,0 @@
-data "aws_secretsmanager_secret" "ass" {
-  name = var.name
-}
-
-data "aws_secretsmanager_secret_version" "asv" {
-  secret_id = data.aws_secretsmanager_secret.ass.name
-}
--- secrets-manager/secrets/vars.tf
+++
@@ -1,4 +0,0 @@
-variable "name" {
-  type          = string
-  description   = "The name of Secrets to be stored"
-}
