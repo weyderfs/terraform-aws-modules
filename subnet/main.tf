4211fe1d35f15ab6ad693b92da9513510bcfd8d6
Weyder Ferreira <weyder.santos@picpay.com>
2022-06-13 23:07:40 -0300

    removendo itens duplicados

--- subnet/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_subnet" "subnet" {
-  vpc_id                = var.vpc_id
-  cidr_block            = var.cidr_block
-  availability_zone     = var.availability_zone
-
-  tags = var.tags
-  
-}
